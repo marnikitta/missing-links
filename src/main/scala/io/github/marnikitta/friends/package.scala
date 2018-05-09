bc472934360076fc87818bda0da7285cd8567fbf
Nikita Marshalkin <marnikitta@gmail.com>
2018-05-09 22:24:06 +0300

    Remove outdated types

--- src/main/scala/io/github/marnikitta/friends/package.scala
+++ src/main/scala/io/github/marnikitta/friends/package.scala
@@ -5,6 +5,4 @@ import org.apache.spark.rdd.RDD
 package object friends {
   type VertexId = Int
   type AdjList = (VertexId, Array[VertexId])
-  type SecondCircleFinder = RDD[AdjList] => RDD[(VertexId, Set[VertexId])]
-  type MetricEvaluator = (RDD[AdjList], RDD[(VertexId, Set[VertexId])]) => RDD[(VertexId, Map[VertexId, Double])]
 }
